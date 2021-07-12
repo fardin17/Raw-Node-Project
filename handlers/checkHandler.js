8c226698c6ba8f9f7fa612b486406e68ad3c25ee
fardin17 <fardin14@cse.pstu.ac.bd>
2021-07-12 22:07:13 +0600

    check crud operation

--- handlers/checkHandler.js
+++ handlers/checkHandler.js
@@ -50,11 +50,11 @@ handler._check.post = (requestProperties, callback) => {
 		data.read('token', token, (err, tokenData) => {
 			if (!err) {
 				const phone = parseJSON(tokenData).phone;
-				data.read('users', phone, (err, userData) => {
+				data.read('users', phone, (err, checkData) => {
 					if (!err) {
 						tokenHandler._token.verify(phone, token, err => {
 							if (!err) {
-								const user = parseJSON(userData);
+								const user = parseJSON(checkData);
 								const userChecks =
 									typeof user.check === 'object' && user.check instanceof Array
 										? user.check
@@ -118,8 +118,203 @@ handler._check.post = (requestProperties, callback) => {
 		});
 	}
 };
-handler._check.get = (requestProperties, callback) => {};
-handler._check.put = (requestProperties, callback) => {};
-handler._check.delete = (requestProperties, callback) => {};
+handler._check.get = (requestProperties, callback) => {
+	const id =
+		typeof requestProperties.queryStringObject.id === 'string' &&
+		requestProperties.queryStringObject.id.trim().length > 0
+			? requestProperties.queryStringObject.id
+			: false;
+	if (id) {
+		const token =
+			typeof requestProperties.headerObject.token === 'string' &&
+			requestProperties.headerObject.token.trim().length === 10
+				? requestProperties.headerObject.token
+				: false;
+		data.read('check', id, (err, checkData) => {
+			if (!err) {
+				tokenHandler._token.verify(parseJSON(checkData).phone, token, err => {
+					if (!err) {
+						callback(200, parseJSON(checkData));
+					} else {
+						callback(403, {
+							Message: 'Authentication Error!',
+						});
+					}
+				});
+			} else {
+				callback(400, {
+					Message: 'Your request is invalid!',
+				});
+			}
+		});
+	} else {
+		callback(400, {
+			Message: 'Your request is invalid!',
+		});
+	}
+};
+handler._check.put = (requestProperties, callback) => {
+	const id =
+		typeof requestProperties.body.id === 'string' &&
+		requestProperties.body.id.trim().length > 0
+			? requestProperties.body.id
+			: false;
+	let protocol =
+		typeof requestProperties.body.protocol === 'string' &&
+		['http', 'https'].indexOf(requestProperties.body.protocol) > -1
+			? requestProperties.body.protocol
+			: false;
+	let url =
+		typeof requestProperties.body.url === 'string' &&
+		requestProperties.body.url.trim().length > 0
+			? requestProperties.body.url
+			: false;
+	let method =
+		typeof requestProperties.body.method === 'string' &&
+		['POST', 'GET', 'PUT', 'DELETE'].indexOf(requestProperties.body.method) > -1
+			? requestProperties.body.method
+			: false;
+	let successCode =
+		typeof requestProperties.body.successCode === 'object' &&
+		requestProperties.body.successCode instanceof Array
+			? requestProperties.body.successCode
+			: false;
+	let timeOut =
+		typeof requestProperties.body.timeOut === 'number' &&
+		requestProperties.body.timeOut % 1 === 0 &&
+		requestProperties.body.timeOut >= 1 &&
+		requestProperties.body.timeOut <= 5
+			? requestProperties.body.timeOut
+			: false;
+	const token =
+		typeof requestProperties.headerObject.token === 'string' &&
+		requestProperties.headerObject.token.trim().length === 10
+			? requestProperties.headerObject.token
+			: false;
+	if (id) {
+		if (protocol || url || method || successCode || timeOut) {
+			data.read('check', id, (err, cData) => {
+				const checkData = { ...parseJSON(cData) };
+				if (!err) {
+					tokenHandler._token.verify(checkData.phone, token, err => {
+						if (!err) {
+							if (protocol) {
+								checkData.protocol = protocol;
+							}
+							if (url) {
+								checkData.url = url;
+							}
+							if (method) {
+								checkData.method = method;
+							}
+							if (successCode) {
+								checkData.successCode = successCode;
+							}
+							if (timeOut) {
+								checkData.timeOut = timeOut;
+							}
+							data.update('check', id, checkData, err => {
+								if (!err) {
+									callback(200, checkData);
+								} else {
+									callback(500, {
+										message: 'There was an error in the server side!',
+									});
+								}
+							});
+						} else {
+							callback(403, {
+								message: 'Authentication error!',
+							});
+						}
+					});
+				} else {
+					callback(400, {
+						message: 'Your request is invalid',
+					});
+				}
+			});
+		} else {
+			callback(400, {
+				message: 'Your provided data is not valid!',
+			});
+		}
+	} else {
+		callback(400, {
+			message: 'Your Phone number is not valid!',
+		});
+	}
+};
+handler._check.delete = (requestProperties, callback) => {
+	const id =
+		typeof requestProperties.queryStringObject.id === 'string' &&
+		requestProperties.queryStringObject.id.trim().length > 0
+			? requestProperties.queryStringObject.id
+			: false;
+	if (id) {
+		const token =
+			typeof requestProperties.headerObject.token === 'string' &&
+			requestProperties.headerObject.token.trim().length === 10
+				? requestProperties.headerObject.token
+				: false;
+		data.read('check', id, (err, cData) => {
+			const checkData = { ...parseJSON(cData) };
+			if (!err) {
+				tokenHandler._token.verify(checkData.phone, token, err => {
+					if (!err) {
+						data.delete('check', id, err => {
+							if (!err) {
+								data.read('users', checkData.phone, (err, uData) => {
+									const userData = { ...parseJSON(uData) };
+									if (!err && userData) {
+										const userCheck =
+											typeof userData.check === 'object' &&
+											userData.check instanceof Array
+												? userData.check
+												: [];
+										const checkPosition = userCheck.indexOf(id);
+										userCheck.splice(checkPosition, 1);
+										userData.check = userCheck;
+										data.update('users', userData.phone, userData, err => {
+											if (!err) {
+												callback(200, userData);
+											} else {
+												callback(500, {
+													Error: 'Server side error!',
+												});
+											}
+										});
+									} else {
+										callback(404, {
+											Error: 'User is invalid!',
+										});
+									}
+								});
+							} else {
+								callback(500, {
+									Error: 'Server side error!',
+								});
+							}
+						});
+					} else {
+						console.log('Authentication error');
+						callback(403, {
+							Error: 'Authentication error',
+						});
+					}
+				});
+			} else {
+				console.log('Authentication error');
+				callback(400, {
+					Message: 'Your provided ID is invalid!',
+				});
+			}
+		});
+	} else {
+		callback(400, {
+			Message: 'Your request is invalid!',
+		});
+	}
+};
 
 module.exports = handler;
