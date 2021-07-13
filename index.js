e70f1d884ea73c9f814ab376932f7351acbe497a
fardin17 <fardin14@cse.pstu.ac.bd>
2021-07-13 23:38:28 +0600

    Send twilio sms

--- index.js
+++ index.js
@@ -5,20 +5,12 @@
  * Date: 07/09/21
  *
  */
-const http = require('http');
-const { handleReqRes } = require('./helpers/handleResReq');
-const environment = require('./helpers/environments');
-const { sendTwilioSms } = require('./helpers//notification');
+const worker = require('./lib/worker');
+const server = require('./lib/server');
 
 const app = {};
-sendTwilioSms('01720276336', 'Hello, Fardin', err => {
-	console.log(err);
-});
-app.config = {};
-app.createServer = () => {
-	const server = http.createServer(handleReqRes);
-	server.listen(environment.port, () => {
-		console.log('Server listening at port at ', environment.port);
-	});
+app.init = () => {
+	worker.int();
+	server.init();
 };
-app.createServer();
+app.init();
